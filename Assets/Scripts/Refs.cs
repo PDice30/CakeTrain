fe799aabf60323dea18007954e44179c2b2a5e26
Phildo <phildo211@gmail.com>
2022-02-19 17:42:11 -0600

    enemy/cart assets in place

--- Assets/Scripts/Refs.cs
+++ Assets/Scripts/Refs.cs
@@ -10,15 +10,29 @@ enum TileIds : int
     TILE_ID_COUNT,
 }
 
+enum EnemyIds : int
+{
+    ENEMY_ID_BAT = 0,
+    ENEMY_ID_GOBLIN,
+    ENEMY_ID_COUNT,
+}
+
 public class Refs : MonoBehaviour
 {
     public GameObject player;
+    public GameObject cartEngine;
+    public GameObject cartCraft;
 
     public GameObject tileGrass;
     public GameObject tileDirt;
     public GameObject tileWater;
     [HideInInspector]
     public GameObject[] tiles;
+
+    public GameObject structureTree;
+    public GameObject structureIronDeposit;
+    public GameObject structureBedrock;
+    public GameObject structureTrack;
 
     public GameObject objectWood;
     public GameObject objectIron;
@@ -28,10 +42,9 @@ public class Refs : MonoBehaviour
     public GameObject objectWoodenPick;
     public GameObject objectIronPick;
 
-    public GameObject structureTree;
-    public GameObject structureIronDeposit;
-    public GameObject structureBedrock;
-    public GameObject structureTrack;
+    public GameObject enemyBat;
+    public GameObject enemyGoblin;
+    public GameObject[] enemies;
 
     public void Initialize()
     {
@@ -44,6 +57,17 @@ public class Refs : MonoBehaviour
                 case (int)TileIds.TILE_ID_WATER: tiles[i] = tileWater; break;
                 case (int)TileIds.TILE_ID_GRASS: tiles[i] = tileGrass; break;
                 default: Debug.Log("failed to place tile!"); break;
+            }
+        }
+
+        enemies = new GameObject[(int)EnemyIds.ENEMY_ID_COUNT];
+        for(int i = 0; i < (int)EnemyIds.ENEMY_ID_COUNT; i++)
+        {
+            switch(i)
+            {
+                case (int)EnemyIds.ENEMY_ID_BAT:  enemies[i] = enemyBat; break;
+                case (int)EnemyIds.ENEMY_ID_GOBLIN: enemies[i] = enemyGoblin; break;
+                default: Debug.Log("failed to place enemy!"); break;
             }
         }
         
